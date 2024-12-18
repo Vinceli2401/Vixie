2b3bfcef6b1cf160b22e260fb5c1b61fe620a54a
Vince Li <vinceli2401@gmail.com>
2024-12-18 09:43:12 -0600

    faet: introduce gravity mechanism that is still buggy

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ const {
   Menu,
   nativeImage,
   ipcMain,
+  screen,
 } = require("electron");
 const path = require("path");
 
 let tray = null;
 let mainWindow;
 let settingsWindow;
+let gravityEnabled = false;
+let gravityInterval = null;
 
-// Main window: index.html
+// Main Window: Vixie
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 200,
@@ -31,11 +34,16 @@ function createWindow() {
   mainWindow.loadFile(path.join(__dirname, "index.html"));
 }
 
-// Setting window: setting.html
+// Settings Window
 function openSettingsWindow() {
+  if (settingsWindow) {
+    settingsWindow.focus();
+    return;
+  }
+
   settingsWindow = new BrowserWindow({
     width: 550,
-    height: 550, // Increased height
+    height: 550,
     title: "Settings",
     resizable: false,
     webPreferences: {
@@ -45,19 +53,25 @@ function openSettingsWindow() {
   });
 
   settingsWindow.loadFile(path.join(__dirname, "settings.html"));
+
+  settingsWindow.on("closed", () => {
+    settingsWindow = null;
+  });
 }
 
+// Tray and Menu
 function createTray() {
   const iconPath = path.join(__dirname, "assets", "app-icon.png");
   const icon = nativeImage
     .createFromPath(iconPath)
     .resize({ width: 16, height: 16 });
-  icon.setTemplateImage(true);
 
   tray = new Tray(icon);
+
   const contextMenu = Menu.buildFromTemplate([
     { label: "Settings", click: () => openSettingsWindow() },
-    { label: "Flip", click: () => flipPet() },
+    { label: "Flip Pet", click: () => flipPet() },
+    { label: "Toggle Gravity", click: () => toggleGravity() },
     { label: "Show Pet", click: () => mainWindow.show() },
     { label: "Hide Pet", click: () => mainWindow.hide() },
     { type: "separator" },
@@ -68,14 +82,46 @@ function createTray() {
   tray.setContextMenu(contextMenu);
 }
 
-// Horizontal flip
+// Flip Pet Logic
 function flipPet() {
   if (mainWindow) {
     mainWindow.webContents.send("flip-pet");
   }
 }
 
-// Listen for updates from settings
+// Gravity Logic
+function toggleGravity() {
+  gravityEnabled = !gravityEnabled;
+
+  if (gravityEnabled) {
+    startGravity();
+  } else {
+    stopGravity();
+  }
+}
+
+function startGravity() {
+  const bounds = mainWindow.getBounds();
+  const screenHeight = screen.getPrimaryDisplay().workAreaSize.height;
+
+  gravityInterval = setInterval(() => {
+    if (bounds.y + bounds.height < screenHeight) {
+      bounds.y += 10;
+      mainWindow.setBounds(bounds);
+    } else {
+      stopGravity();
+    }
+  }, 50);
+}
+
+function stopGravity() {
+  if (gravityInterval) {
+    clearInterval(gravityInterval);
+    gravityInterval = null;
+  }
+}
+
+// IPC for GIF Updates
 ipcMain.on("update-gif", (event, gifUrl) => {
   if (mainWindow) {
     mainWindow.webContents.send("set-gif", gifUrl);
