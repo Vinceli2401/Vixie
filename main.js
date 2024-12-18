88e4d07f630dd9df703f01f39ee033e05eae451a
Vince Li <vinceli2401@gmail.com>
2024-12-18 08:41:10 -0600

    feat: add flipping logic

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-const { app, BrowserWindow, Tray, Menu, nativeImage } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  Menu,
+  nativeImage,
+  ipcMain,
+} = require("electron");
 const path = require("path");
 
-let tray = null; // Declare tray globally
+let tray = null;
 let mainWindow;
+let settingsWindow;
 
+// Main window: index.html
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 200,
@@ -22,16 +31,33 @@ function createWindow() {
   mainWindow.loadFile(path.join(__dirname, "index.html"));
 }
 
+// Setting window: setting.html
+function openSettingsWindow() {
+  settingsWindow = new BrowserWindow({
+    width: 550,
+    height: 550, // Increased height
+    title: "Settings",
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  settingsWindow.loadFile(path.join(__dirname, "settings.html"));
+}
+
 function createTray() {
-  const iconPath = path.join(__dirname, "assets/app-icon.png");
+  const iconPath = path.join(__dirname, "assets", "app-icon.png");
   const icon = nativeImage
     .createFromPath(iconPath)
     .resize({ width: 16, height: 16 });
   icon.setTemplateImage(true);
-  tray = new Tray(icon);
 
+  tray = new Tray(icon);
   const contextMenu = Menu.buildFromTemplate([
     { label: "Settings", click: () => openSettingsWindow() },
+    { label: "Flip", click: () => flipPet() },
     { label: "Show Pet", click: () => mainWindow.show() },
     { label: "Hide Pet", click: () => mainWindow.hide() },
     { type: "separator" },
@@ -42,19 +68,19 @@ function createTray() {
   tray.setContextMenu(contextMenu);
 }
 
-function openSettingsWindow() {
-  const settingsWindow = new BrowserWindow({
-    width: 300,
-    height: 200,
-    title: "Settings",
-    resizable: false,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-  settingsWindow.loadFile(path.join(__dirname, "settings.html"));
+// Horizontal flip
+function flipPet() {
+  if (mainWindow) {
+    mainWindow.webContents.send("flip-pet");
+  }
 }
+
+// Listen for updates from settings
+ipcMain.on("update-gif", (event, gifUrl) => {
+  if (mainWindow) {
+    mainWindow.webContents.send("set-gif", gifUrl);
+  }
+});
 
 app.whenReady().then(() => {
   createWindow();
