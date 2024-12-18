aae689fa7f5b949c076324a5d7925cc51d851deb
Vince Li <vinceli2401@gmail.com>
2024-12-17 19:00:09 -0600

    fix: enforce proportional resizing for diagonal window resizing

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const sizeOf = require("image-size");
+const imagePath = path.join(__dirname, "assets/celebi.gif");
+const { width, height } = sizeOf(imagePath);
 
 function createWindow() {
+  const aspectRatio = width / height;
+
   const win = new BrowserWindow({
-    width: 300,
-    height: 300,
-    transparent: true, // Make the window background transparent
-    frame: false, // Remove window frame
-    alwaysOnTop: true, // Keep the window on top
-    resizable: false,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
+    width,
+    height,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true,
+    resizable: true,
+    webPreferences: { nodeIntegration: true, contextIsolation: false },
   });
 
-  win.setIgnoreMouseEvents(true); // Makes the window "click-through"
-  win.loadFile("index.html"); // Load the frontend file
+  win.on("will-resize", (event, newBounds) => {
+    const { width, height } = newBounds;
+    const newHeight = Math.round(width / aspectRatio);
+    const newWidth = Math.round(height * aspectRatio);
+
+    if (width / height > aspectRatio) {
+      event.preventDefault();
+      win.setBounds({ width: newWidth, height });
+    } else {
+      event.preventDefault();
+      win.setBounds({ width, height: newHeight });
+    }
+  });
+
+  win.loadFile("index.html");
 }
 
 app.whenReady().then(createWindow);
